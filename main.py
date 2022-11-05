31423a6cbc350d718b393e77383f517271c3e95b
alicebrugnoli <79971625+alicebrugnoli@users.noreply.github.com>
2022-11-05 16:24:59 +0100

    csv

--- main.py
+++ main.py
@@ -9,10 +9,11 @@ def converter(x):
 
 converters = {"Impressions": converter}
 interactions_and_impressions = pd.read_csv(
-    '/Users/alicebrugnoli/Desktop/dataset Recsys/interactions_and_impressions.csv',
+    '../input/interactions-and-impressions/interactions_and_impressions.csv',
     converters=converters)
 interactions_and_impressions.columns = ["UserID", "ItemID", "Impressions", "Data"]
 
+"""""
 data_ICM_length = pd.read_csv('/Users/alicebrugnoli/Desktop/dataset Recsys/data_ICM_length.csv',
                               dtype={0: int, 1: int, 2: int},
                               engine='python')
@@ -22,3 +23,4 @@ data_ICM_type = pd.read_csv('/Users/alicebrugnoli/Desktop/dataset Recsys/data_IC
                             dtype={0: int, 1: int, 2: int},
                             engine='python')
 data_ICM_length.columns = ["ItemID", "FeatureID", "Data"]
+"""
